9996b1400a780c2b096d7dbbd33562fceb6c1603
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2021-10-24 15:45:41 -0300

    front-end para consumo da api

---
+++ front-endAPI/primeng-sandbox/src/app/entity/Tabela.ts
@@ -0,0 +1,7 @@
+export class Tabela {
+    public id : number
+    public autorizador: string
+    public autorizacao: string
+    public retornoAutorizacao: string
+    public inutilizacao: string
+}
