a763081e7a5a9acf8074993b2d06bf7aa2639b89
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2021-10-26 22:17:05 -0300

    inicializando trabalhos da api

---
+++ front-end-api/src/app/model/tabela.ts
@@ -0,0 +1,12 @@
+export class Tabela{
+    public id: number
+    public autorizador: string
+    public autorizacao: string
+    public retornoAutorizacao: string
+    public inutilizacao: string
+    public consultaProtocolo: string
+    public statusServico: string
+    public tempoMedio: string
+    public consultaCadastro: string
+    public recepcaoEvento: string
+}
